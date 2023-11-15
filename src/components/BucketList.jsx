7e8a08ed456eed09fd0f05cf6f9997cfe0b0bb4f
Logan Garland <legarland@gmail.com>
2023-11-14 21:58:15 -0700

    updated page header

--- src/components/BucketList.jsx
+++ src/components/BucketList.jsx
@@ -61,7 +61,7 @@ function BucketList() {
 
   return (
     <div>
-      <h1>What is on your bucket list?</h1>
+      <h1>What is on your bucket list???????</h1>
       <BucketForm onSubmit={addBucketItem} />
       <Bucket
         bucket={bucket}
